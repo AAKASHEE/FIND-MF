6d658d7bbe5cc8ca4038d0e47b5dd55210bfc5ec
AAKASHE <aakashpatra253@gmail.com>
2026-05-07 19:57:03 +0530

    add locations storage

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,57 @@
 const express = require("express");
 const cors = require("cors");
+const fs = require("fs");
 
 const app = express();
-
-const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
 
+const FILE = "locations.json";
+
+if (!fs.existsSync(FILE)) {
+  fs.writeFileSync(FILE, "[]");
+}
+
 app.post("/location", (req, res) => {
 
+  const data = req.body;
+
+  const mapsLink =
+    `https://maps.google.com/?q=${data.latitude},${data.longitude}`;
+
+  const locations =
+    JSON.parse(fs.readFileSync(FILE));
+
+  locations.push({
+    ...data,
+    maps: mapsLink
+  });
+
+  fs.writeFileSync(
+    FILE,
+    JSON.stringify(locations, null, 2)
+  );
+
   console.log("LOCATION RECEIVED:");
-  console.log(req.body);
+  console.log(data);
+
+  console.log("GOOGLE MAPS:");
+  console.log(mapsLink);
 
   res.json({
     success: true,
-    message: "Location saved"
+    maps: mapsLink
   });
+
+});
+
+app.get("/locations", (req, res) => {
+
+  const locations =
+    JSON.parse(fs.readFileSync(FILE));
+
+  res.json(locations);
 
 });
 
@@ -25,6 +60,8 @@ app.get("/", (req, res) => {
   res.send("Backend running");
 
 });
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
 
